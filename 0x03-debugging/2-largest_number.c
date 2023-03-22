587635253c62ffbe00ca7c25d124ec2df45f8a1f
latifaaaaa <latifa.moussafir123@gmail.com>
2023-03-22 14:03:55 -0700

    task 22

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,43 +9,28 @@
 */
 
 int largest_number(int a, int b, int c)
-
+i
 {
-
 int largest;
-
-if (a > b && a > c)
-
+if (a > b && b > c)
 {
-
 largest = a;
-
 }
-
-else if (a > b && c > a)
-
+else if (a > c && c > b)
 {
-
-largest = c;
-
+largest = a;
 }
-
-else if (b > c)
-
+else if (b > a && a > c)
 {
-
 largest = b;
-
 }
-
-else
-
-{
-
+else if (b > c && c > a)
+largest = b;
+else if (c > a && a > b)
 largest = c;
-
+else
+{
+largest = c;
 }
-
 return (largest);
-
 }
